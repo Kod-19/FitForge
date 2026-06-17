edd9e28aa105a03f256286503f45d66cca25e8e5
Kod-19 <dawsonkwame9@gmail.com>
2026-06-17 22:27:43 +0000

    Initialized repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black min-h-screen'>
+      <div className=''>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
