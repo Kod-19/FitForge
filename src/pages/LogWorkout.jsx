8a47f552a856ab0e4925714c326325cd10c89296
Kod-19 <dawsonkwame9@gmail.com>
2026-06-18 22:59:16 +0000

    added components, pages and data files to project folder

---
+++ src/pages/LogWorkout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LogWorkout = () => {
+  return (
+    <div>LogWorkout</div>
+  )
+}
+
+export default LogWorkout
---
+++ src/pages/WorkoutBuilder.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WorkoutBuilder = () => {
+  return (
+    <div>WorkoutBuilder</div>
+  )
+}
+
+export default WorkoutBuilder
