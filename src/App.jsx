231065810f177776ecc2fbd1894ef606618d6609
Kod-19 <dawsonkwame9@gmail.com>
2026-06-19 16:56:38 +0000

    set up the app.jsx file - included the routes, components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Login from './pages/Login'
+import Dashboard from "./pages/Dashboard";
+import WorkoutBuilder from "./pages/WorkoutBuilder";
+import ExerciseLibrary from "./pages/ExerciseLibrary";
+import LogWorkout from "./pages/LogWorkout";
+import Progress from "./pages/Progress";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <div className='bg-black min-h-screen'>
-      <div className=''>
-
-      </div>
+    <div>
+      <BrowserRouter>
+        <Toaster position='top-right' />
+        <Routes>
+          <Route path='/login' element={<Login />} />
+          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+          <Route path="/builder" element={<ProtectedRoute><WorkoutBuilder /></ProtectedRoute>} />
+          <Route path="/library" element={<ProtectedRoute><ExerciseLibrary /></ProtectedRoute>} />
+          <Route path="/log" element={<ProtectedRoute><LogWorkout /></ProtectedRoute>} />
+          <Route path="/progress" element={<ProtectedRoute><Progress /></ProtectedRoute>} />
+          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
